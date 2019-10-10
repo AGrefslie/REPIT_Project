9f06ffc9f70a921b5d92b1c7b4c646fefa2511db
Axel Grefslie <agrefs97@gmail.com>
2019-10-10 12:22:06 +0200

    Fixed xml error
    Fixed xml error

--- app/src/main/java/com/example/repit_project/MainActivity.kt
+++ app/src/main/java/com/example/repit_project/MainActivity.kt
@@ -5,9 +5,12 @@ import android.os.Bundle
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
 import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupWithNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.repit_project.Data.TestData
+import com.example.repit_project.R.id.nav_host_fragment
 import com.example.repit_project.RecyclerViewAdapter.QuizAdapter
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.fragment_home.recycler_view
 
@@ -20,12 +23,22 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+
         val navController = Navigation.findNavController(this, R.id.nav_host_fragment)
         setUpBottomNavMenu(navController)
 
         initRecyclerView()
         addDataSet()
     }
+
+    /*
+    private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { menuItem ->  
+        when (menuItem.itemId){
+            R.id.destination_collections -> {
+               supportFragmentManager.beginTransaction().replace(R.id.)
+            }
+        }
+    }*/
 
     private fun setUpBottomNavMenu(navController : NavController) {
         bottom_navigation?.let{
@@ -34,6 +47,7 @@ class MainActivity : AppCompatActivity() {
     }
 
     //Legger til data fra testklassen
+
     private fun addDataSet() {
         val data = TestData.createDataSet()
         quizAdapter.submitList(data)
