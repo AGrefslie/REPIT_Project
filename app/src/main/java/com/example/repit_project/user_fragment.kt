f6d6eff11a41799573aecd4ca09d811fe2ded3e2
Axel Grefslie <agrefs97@gmail.com>
2019-10-18 01:13:57 +0200

    Tried to add imageselecter
    tried to add imageselecter for background image in userprofile (feature commented out)

--- app/src/main/java/com/example/repit_project/user_fragment.kt
+++ app/src/main/java/com/example/repit_project/user_fragment.kt
@@ -1,20 +1,25 @@
 package com.example.repit_project
 
 
+import android.app.PendingIntent
 import android.content.Intent
 import android.content.Intent.ACTION_PICK
 import android.net.Uri
+import android.os.Build
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.annotation.RequiresApi
 import com.google.firebase.auth.FirebaseAuth
 import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.fragment_user.*
 
 
 class user_fragment : Fragment() {
+
+    //private val pickImage = 1
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -25,23 +30,26 @@ class user_fragment : Fragment() {
         return inflater.inflate(R.layout.fragment_user, container, false)
     }
 
+    @RequiresApi(Build.VERSION_CODES.LOLLIPOP_MR1)
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         setProfileDetails()
 
-        userBackImage.setOnClickListener {
+        /*userBackImage.setOnClickListener {
 
             val galleryIntent = Intent(ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+            //val pendingIntent = PendingIntent.getBroadcast(context, 0, galleryIntent, PendingIntent.FLAG_UPDATE_CURRENT)
 
-            startActivity(galleryIntent)
+            startActivityForResult(Intent.createChooser(galleryIntent, "Sellect image", pickImage))
 
-            val loadedImage = galleryIntent.data
+        }*/
+    }
 
-            Picasso.get().load(loadedImage.toString()).into(userBackImage)
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
 
-            backImageUrl.setText(loadedImage.toString())
-        }
+
     }
 
     fun setProfileDetails() {
