e1ffb85b501f4406c76a1c833dd56706cb65adcd
Axel Grefslie <agrefs97@gmail.com>
2019-10-27 17:13:06 +0100

    Added conection to firestore
    Added conection to firestore and added test data

--- app/src/main/java/com/example/repit_project/create_test_fragment.kt
+++ app/src/main/java/com/example/repit_project/create_test_fragment.kt
@@ -6,14 +6,22 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.repit_project.Models.Quiz
+import com.google.firebase.firestore.FirebaseFirestore
 
 
 class create_test_fragment : Fragment() {
+
+    private lateinit var db : FirebaseFirestore
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+
+        db = FirebaseFirestore.getInstance()
+
+
 
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_create_test, container, false)
--- app/src/main/java/com/example/repit_project/home_fragment.kt
+++ app/src/main/java/com/example/repit_project/home_fragment.kt
@@ -5,23 +5,27 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
-import androidx.navigation.Navigation
-import androidx.navigation.findNavController
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
 import com.example.repit_project.Models.Question
 import com.example.repit_project.Models.Quiz
 import com.example.repit_project.RecyclerViewAdapter.QuizAdapter
+import com.google.firebase.firestore.CollectionReference
+import com.google.firebase.firestore.FirebaseFirestore
 import kotlinx.android.synthetic.main.fragment_home.*
-import kotlinx.android.synthetic.main.layout_listitem.*
-import kotlinx.android.synthetic.main.layout_listitem.view.*
+
 
 class home_fragment : Fragment() {
 
+    private lateinit var db : FirebaseFirestore
+    private lateinit var collectionQuizes : CollectionReference
+
+    var quizList = ArrayList<Quiz>()
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        db = FirebaseFirestore.getInstance()
+        collectionQuizes = db.collection("Quizes")
+
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_home, container, false)
     }
@@ -29,17 +33,21 @@ class home_fragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+
         //Setup FAB navigation
         FAB_CreateTestButton.setOnClickListener {
             val action = home_fragmentDirections.actionDestinationHomeToDestinationCreateTest()
             findNavController().navigate(action)
         }
 
+        //genereateTestData()
+
         recycler_view.apply {
             layoutManager = LinearLayoutManager(activity)
             adapter = QuizAdapter(QuizList)
         }
     }
+
 
     companion object {
         private val QuestionList1 = listOf(
@@ -60,6 +68,26 @@ class home_fragment : Fragment() {
             //Quiz("Test Quiz", "This is just a test", "https://images.unsplash.com/photo-1524646432175-d58115a8a854?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80", false, QuestionList),
             //Quiz("Test Quiz", "This is just a test", "https://images.unsplash.com/photo-1524646432175-d58115a8a854?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80", false, QuestionList)
         )
+    }
+
+    fun genereateTestData() {
+        /*var QuestionList1 = ArrayList<Question>()
+        var QuestionList2 = ArrayList<Question>()
+
+        QuestionList1.add(Question("Hvem er kaptein for Manchester United?", "Ashley Young"))
+        QuestionList1.add(Question("Hvem er norges største tallent i golf i dag?", "Viktor Hovland"))
+        QuestionList1.add(Question("Hvem er manager for Manchester United i 2019", "Ole Gunnar Solskjær"))
+
+        QuestionList2.add(Question("Hva er Hei på Spansk?", "Hola"))
+        QuestionList2.add(Question("Hva er Hei på Engelsk?", "Hello"))
+
+        quizList.add(Quiz("Hello Quiz", "Hello in different languages", "https://i.ytimg.com/vi/kJ2dr9jAThY/maxresdefault.jpg", false, QuestionList1))
+        quizList.add(Quiz("MUFC & GOLF", "No description needed", "https://www.soccerpro.com/wp-content/uploads/2018/02/ManchesterUnited_1280x800.jpg", false, QuestionList2))
+        */
+
+        for (Quiz in QuizList) {
+            collectionQuizes.add(Quiz)
+        }
     }
 
 }
